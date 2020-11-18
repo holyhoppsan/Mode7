972f65d086c40d6ca73982af48f94b7486ac6b4a
Holyhoppsan <daniel.hall@live.se>
2020-11-18 21:44:59 +0000

    Added SDL support

--- mode7.go
+++ mode7.go
@@ -2,8 +2,43 @@ package main
 
 import (
 	"fmt"
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
 	fmt.Println("Mode7 begins!")
+
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Mode 7", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 640, 480, sdl.WINDOW_SHOWN)
+
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	surface.FillRect(nil, 0)
+
+	rect := sdl.Rect{0, 0, 200, 200}
+	surface.FillRect(&rect, 0xff00ff00)
+	window.UpdateSurface()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				fmt.Println("Quit")
+				running = false
+				break
+			}
+		}
+	}
 }
